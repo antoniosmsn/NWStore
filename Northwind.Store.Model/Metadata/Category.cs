e45035c7c7c563c1c9781c1da591423f98322646
Manuel <antoniosmsn@hotmail.com>
2016-03-05 13:52:24 -0600

    Carga los archivos que tenia cargados anterior mente

---
+++ Northwind.Store.Model/Metadata/Category.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using Northwind.Store.Model.Resource;
+
+namespace Northwind.Store.Model
+{
+    [MetadataType(typeof(CategoryMetadata))]
+    public partial class Category
+    {
+        public class CategoryMetadata
+        {
+            [Required(ErrorMessageResourceType = typeof(GeneralMessages), ErrorMessageResourceName = "CategoryNameRequired")]
+            [StringLength(15, MinimumLength = 3, ErrorMessageResourceType = typeof(GeneralMessages), ErrorMessageResourceName = "CategoryNameLength")]
+            [Display(Name = "CategoryNameTitle", ResourceType = typeof(GeneralMessages))]
+            public string CategoryName { get; set; }
+
+
+            [Required(ErrorMessageResourceType = typeof(GeneralMessages), ErrorMessageResourceName = "CategoryDescriptionRequired")]
+            [MinLength(30, ErrorMessageResourceType = typeof(GeneralMessages), ErrorMessageResourceName = "CategoryDescriptionLength")]
+            [Display(Name = "CategoryDescriptionTitle", ResourceType = typeof(GeneralMessages))]
+            public string Description { get; set; }
+        }
+    }
+}
